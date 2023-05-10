9c88995213460257b2476010fb5d75cf6cae5d28
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-05-10 12:55:33 +0200

    lava rising stuff

--- Assets/scripts/Kiefer/LavaRise.cs
+++ Assets/scripts/Kiefer/LavaRise.cs
@@ -1,21 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LavaRise : MonoBehaviour
 {
-    void Start()
+    bool started = true;
+
+    [SerializeField] GameObject MaxHight;
+
+    public int MaxTime;
+    int currTime = 1;
+
+    Vector3 CurrentPos;
+
+    float hightDifPerSecond;
+
+    private void Start()
     {
-        
+        float hightDif = MathF.Abs(this.gameObject.transform.position.y - MaxHight.transform.position.y);
+        hightDifPerSecond = hightDif / MaxTime;
     }
 
     void Update()
     {
-        
+        if (started)
+        {
+            while (MaxTime >= currTime)
+            {
+                InvokeRepeating("LavaIncreases", 1f, 1f);
+            }
+            if (MaxTime < currTime)
+            {
+                CancelInvoke();
+            }
+        }
     }
-    
+
+    void LavaIncrease()
+    {
+        currTime++;
+        Debug.Log(Time.time);
+        //CurrentPos = this.gameObject.transform.position;
+    }
+
     public void StartRizing()
     {
-
+        started = true;
     }
 }
