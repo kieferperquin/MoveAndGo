9872e47c34ce7952c6e0d73b0598e8dc0cd4d1b3
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-05-24 10:59:09 +0200

    falling hazards start

---
+++ Assets/Scripts/Kiefer/FH/FallingHazardManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingHazardManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject FH;
+    int lanes;
+    [SerializeField]
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
