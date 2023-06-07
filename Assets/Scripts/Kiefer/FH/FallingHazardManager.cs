bfa92aebd5ef7a2922c4abaa4b2bae346ca8f9f2
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-06-07 12:58:47 +0200

    made warning mechanic warning

--- Assets/Scripts/Kiefer/FH/FallingHazardManager.cs
+++ Assets/Scripts/Kiefer/FH/FallingHazardManager.cs
@@ -1,16 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FallingHazardManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] FH;
     [SerializeField] private GameObject[] lanes;
-    
+
+    [SerializeField] private Color[] colorCodes;
+
+    [SerializeField] private GameObject warningText;
+
     [SerializeField] private int maxTime;
     [SerializeField] private int minTime;
 
-    private float spawnInterval;
+    [SerializeField] private float warningTime;
+
+    private int spawnLocation;
+
+    private float spawnInt;
     private float timer;
     
     private bool spawn;
@@ -18,45 +25,69 @@ public class FallingHazardManager : MonoBehaviour
 
     private void Start()
     {
-        spawnInterval = Random.Range(minTime, maxTime);
+        if (lanes.Length != colorCodes.Length)
+        {
+            Debug.LogError("colors != lanes");
+        }
+        CalSpawnInt();
+        SetSpawnLoc();
     }
 
     private void Update()
     {
-        if (started)
+        if (!started)
+            return;
+
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInt)
         {
-            timer += Time.deltaTime;
-
-            if (timer >= spawnInterval)
-            {
-                spawn = true;
-                spawnInterval = Random.Range(minTime, maxTime);
-                timer = 0f;
-            }
+            spawn = true;
+            CalSpawnInt();
+            timer = 0f;
         }
-
+        if (timer >= spawnInt - warningTime)
+        {
+            SpawnWarningIndicator();
+        }
         if (spawn)
         {
             SpawnFH();
+            SetSpawnLoc();
             spawn = false;
         }
     }
-
+    void SetSpawnLoc()
+    {
+        spawnLocation = Random.Range(0, lanes.Length);
+        Debug.Log(spawnLocation);
+    }
+    void CalSpawnInt()
+    {
+        spawnInt = Random.Range(minTime, maxTime);
+    }
     void SpawnFH()
     {
-        Instantiate(FH[Random.Range(0, FH.Length)], CalRowToSpawn(Random.Range(0, lanes.Length)), Quaternion.identity);
+        warningText.SetActive(false);
+        Instantiate(FH[Random.Range(0, FH.Length)], CalRowToSpawn(), Quaternion.identity);
     }
-
-    Vector3 CalRowToSpawn(int random)
+    void SpawnWarningIndicator()
     {
-        return lanes[random].transform.position;
+        warningText.SetActive(true);
+        SetTextColor(warningText);
     }
-
+    void SetTextColor(GameObject txtObject)
+    {
+        txtObject.GetComponent<Text>().color = colorCodes[spawnLocation];
+    }
+    Vector3 CalRowToSpawn()
+    {
+        return lanes[spawnLocation].transform.position;
+    }
     public void StartFH()
     {
         started = true;
     }
-
     public void StopFH()
     {
         started = false;
